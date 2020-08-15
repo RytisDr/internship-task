7dfbc98b932e5df1e05ed6e38955d8715674fd11
RytisDr <r.drazdauskas@gmail.com>
2020-08-16 01:47:07 +0300

    Fetches and displays film names in h1.

--- star-wars/src/App.js
+++ star-wars/src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./style/style.scss";
+import Films from "./components/Films";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <Router>
+            <Switch>
+              <Route path="/character"></Route>
+            </Switch>
+          </Router>
+        </nav>
       </header>
+      <Films></Films>
     </div>
   );
 }
