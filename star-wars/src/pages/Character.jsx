2cad37f572a123a6497eb5d92531c8bbc0b55645
Mxney <cn@mxney.io>
2020-08-17 23:06:17 +0200

    Added fetchFilms2

--- star-wars/src/pages/Character.jsx
+++ star-wars/src/pages/Character.jsx
@@ -12,6 +12,37 @@ const CharacterPage = () => {
         fetchFilms(character);
       });
 
+      /*
+        I just created a "fetchFilm2"
+
+        The one you made with the promise has some issues if there is a slow loop first, and then a quicker loop.
+        The problem is that they could run at the same time, wich would damage this part:
+        if (index === array.length - 1) resolve();
+
+        What i did instead was to have a simple for loop and then const a rest that uses "awaits" when fetching instead of
+        the "promise" you used.
+
+        This dosent really make it cleaner, but its a bit simpler and runs async that will fix most problems.
+
+        Then also one quick note. In the example here i define the array insid the "fetchFilm2".
+
+        It's kinda optional if you define those things at the beginning or when they are used.
+        Sometimes in long scrips, it can be pretty hard to find the definition when its at the top
+        and you are like 2.000 lines down.
+      */
+
+      const fetchFilms2 = async (character) => {
+        const filmsArray = [];
+        for(let film of character.films){
+          const res = await fetch(film);
+          const data = res.json();
+          filmsArray.push(film.title);
+        }
+        character.films = filmsArray;
+        setCharacter(character);
+
+      }
+
     const fetchFilms = (character) => {
       // Promise to make sure that state is only changed when the array is finished in forEach, resolved.
       const promise = new Promise((resolve, reject) => {
